718e98b4465d02c600297bde9df2b74690f82fc6
reputation <857466032zxr@gmail.com>
2020-07-30 13:57:24 +0800

    new

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,36 +4,40 @@
     <HeaderMenu></HeaderMenu>
     <!--    轮播图-->
     <Carousel></Carousel>
+    <!--    中间的广告标题-->
+    <AdLable></AdLable>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '../components/HelloWorld.vue';
-import HeaderMenu from '../components/HeaderMenu.vue';
-import Carousel from '../components/Carousel.vue';
+  // @ is an alias to /src
+  import HelloWorld from '../components/HelloWorld.vue';
+  import HeaderMenu from '../components/HeaderMenu.vue';
+  import Carousel from '../components/Carousel.vue';
+  import AdLable from '../components/AdLable';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-    HeaderMenu,
-    Carousel,
-  },
-  data() {
-    return {
-      isCollapse: true,
-    };
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld,
+      HeaderMenu,
+      Carousel,
+      AdLable,
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    data() {
+      return {
+        isCollapse: true,
+      };
     },
-  },
-};
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      },
+    },
+  };
 </script>
 
 <style>
